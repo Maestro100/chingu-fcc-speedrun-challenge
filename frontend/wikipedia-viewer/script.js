cc1da3bdc85a4c6c79f7b8c6d1ff1c9395dd52c1
AgathaLynn <amaskelton@verizon.net>
2017-04-27 21:43:48 -0400

    wikipedia viewer - styling and function completed

--- frontend/wikipedia-viewer/script.js
+++ frontend/wikipedia-viewer/script.js
@@ -1,27 +1,34 @@
+
 var search_field = document.getElementById('search-field');
-var search_btn = document.getElementById('search-btn');
 var rand_btn = document.getElementById('rand-btn');
 var search_results = document.getElementById('search-results');
 
+// request articles from wikipedia
 function search(input, success) {
   input = encodeURIComponent(input);
-  var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=revisions&list=search&rvprop=ids&srsearch=" + input + "&srlimit=10&srinfo=totalhits%7Csuggestion%7Crewrittenquery";
-
+  var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts&exintro&exchars=250&exlimit=10&generator=search&gsrsearch=" + input + "&gsrlimit=10&formatversion=2";
   var req = new XMLHttpRequest();
+
   req.open("GET", url);
   req.setRequestHeader('Api-User-Agent', 'Example/1.0');
   req.onload = function() {
     if (req.status === 200) {
       var response = JSON.parse(req.response);
-      success(response.query.search);
+      if (response.query && response.query.pages) {
+        success(response.query.pages);
+      }
+      else {
+        success([]);
+      }
     }
     else {
       console.log('failure');
     }
-  }
+  };
   req.send();
 }
 
+// update webpage with search results
 function updateResults(data) {
   var results = "";
   for (var i = 0; i < data.length; i++) {
@@ -30,16 +37,24 @@ function updateResults(data) {
   search_results.innerHTML = results;
 }
 
+// build div to hold results
 function construct(result) {
-  return '<a href="https://en.wikipedia.org/wiki/' + encodeURIComponent(result.title) + '">' +
-           '<div class="response">' +
+  return '<div class="response">' +
+           '<a href="https://en.wikipedia.org/wiki/' + encodeURIComponent(result.title) + '">' +
              '<h2>' + result.title + '</h2>' +
-             '<p>' + result.snippet + '...</p>' +
-           '</div>' +
-         '</a>';
+             result.extract +
+           '</a>' +
+         '</div>';
 }
 
-
+// listen for key-up event (to conduct search)
 search_field.addEventListener("keyup", function() {
-  search(search_field.value, updateResults);
+  if (search_field.value.length > 0) {
+    search(search_field.value, updateResults);
+  }
+});
+
+// set up random button
+rand_btn.addEventListener("click", function() {
+  window.open("https://en.wikipedia.org/wiki/Special:Random");
 });
